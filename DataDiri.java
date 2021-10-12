0f460bb82c56a7c2e8b3fb41c6be36a02a222d0a
KhairunNisaManurung <khairunnisa.mnr@gmail.com>
2021-10-11 22:20:17 -0700

    project 1

---
+++ DataDiri.java
@@ -0,0 +1,30 @@
+public class DataDiri {
+    public static void main(String args[]){
+        //first name, last name, age, addres, hobi, phone number, makanan kesukaan, berat badan
+        String firstName, lastName, Alamat, hobi, makanankesukaan, phoneNumber;
+        int usia; 
+        double beratBadan;
+
+        firstName = "Khairun Nisa ";
+        lastName = "Manurung";
+        Alamat = "Gangnam, Style";
+        hobi = "Badminton";
+        usia = 21;
+        phoneNumber = "082256785490";
+        beratBadan = 48;
+        makanankesukaan = "Ayam Goreng";
+
+        System.out.println("Nama : " + firstName + lastName);
+        System.out.println("Alamat : " + Alamat);
+        System.out.println("Usia : " + usia);
+        System.out.println("Berat Badan : " + beratBadan + "kg");
+        System.out.println("Hobi : " + hobi);
+        System.out.println("Nomor Telepon: " + phoneNumber);
+        System.out.println("makanankesukaan : " + makanankesukaan);
+
+        
+
+
+
+    }
+}    
